5c0e5d377c9eb4a23a69862cd56940514accc1fd
manish091102 <manish091102@gmail.com>
2025-04-03 14:18:48 +0530

    Logs management

--- routes/route.js
+++ routes/route.js
@@ -6,11 +6,13 @@ const ProductRoutes = require('./productRoute');
 const CustomerRoutes = require('./customerRoutes');
 const UserRoutes = require('./userRoutes');
 const WareHouseRoutes = require('./wareHouseRoutes');
+const logsManagement = require('./logsManagementRoutes');
 
 //allow to use your route files
 router.use('/customers',CustomerRoutes);
 router.use('/products',ProductRoutes);
 router.use('/users',UserRoutes);
 router.use('/wareHouse',WareHouseRoutes);
+router.use('/logsManagement',logsManagement);
 
 module.exports = router;
