c5252865674b88bc1b3c7dd7c5e0cd526b699e89
techteam807 <techcenter@doshionpoly.com>
2025-04-08 15:11:27 +0530

    validtion format warehouse

--- models/wareHouseModel.js
+++ models/wareHouseModel.js
@@ -6,6 +6,14 @@ const WareHouseSchema = new mongoose.Schema(
         type:String,
         required:true,
         unique: true,
+        validate: {
+        validator: function (value) {
+          // return /^[A-Za-z]{4}\d{4}$/.test(value); 
+          return /^\d{4}\.\d{2}\.\d{2}\.[A-Za-z]$/.test(value);
+        },
+        // message: "Product code must have 4 letters followed by 4 digits (e.g., ABCD1234)",
+        message:"WareHouse code must follow the format: 1054.59.25.W"
+      },
     },
   },
   { timestamps: true }
