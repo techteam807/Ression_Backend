75f3fffddef660e15cbc5cd6c44d593e89ba39d7
techteam807 <techcenter@doshionpoly.com>
2025-04-08 15:11:27 +0530

    validtion format warehouse

--- validations/wareHouseValidation.js
+++ validations/wareHouseValidation.js
@@ -2,11 +2,11 @@ const Joi = require("joi");
 
 const createWareHouse = Joi.object({
     wareHouse_code: Joi.string()
-      .pattern(/[A-Za-z]{2}-[A-Za-z]{4}-\d+$/) 
+      .pattern(/^\d+\.\d+\.\d+\.[A-Za-z]$/)
       .required()
       .messages({
         "string.empty": "WareHouse code is required",
-        "string.pattern.base":"WareHouse code must follow the format: AB-ABCD-01"
+        "string.pattern.base":"WareHouse code must follow the format: 1054.59.25.W"
       }),
   });
 
