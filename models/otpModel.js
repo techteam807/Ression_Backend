60ee3e9b8bd5f86336c7f495c9b5e1d1f235684f
techteam807 <techcenter@doshionpoly.com>
2025-04-09 12:30:49 +0530

    add logic

--- models/otpModel.js
+++ models/otpModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const otpSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     otp: { type: String, required: true },
-    expiration: { type: Date, required: true },
+    CreatedAt: { type: Date, default: Date.now, expire: 120 },
 });
 
 module.exports = mongoose.model('Otp', otpSchema);
