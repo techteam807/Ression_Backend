483d4f1bf62d21fac7433899abcd4d707098fc15
techteam807 <techcenter@doshionpoly.com>
2025-04-09 17:47:17 +0530

    add delete product otp

--- routes/superAdminRoute.js
+++ routes/superAdminRoute.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 router.put('/clearProductOfCutomer',SuperAdmin.clearProducts);
 router.patch('/manage-product-status', SuperAdmin.manageProductStatus);
+router.post('/sendOtpProductDelete',SuperAdmin.sendOtpForDeleteProduct);
+router.post('/productDeleteProcess',SuperAdmin.deleteProductProcess);
 
 module.exports = router;
