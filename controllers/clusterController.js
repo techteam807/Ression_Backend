7ba43f2857cdfdd9f2885633e7f8acd4ea42c424
manish091102 <manish091102@gmail.com>
2025-05-02 18:40:06 +0530

    Cluster manage and code updated

---
+++ controllers/clusterController.js
@@ -0,0 +1,43 @@
+
+const { successResponse, errorResponse } = require("../config/response");
+const { getClusteredCustomerLocations, getAllClusters, reassignMultipleCustomersToClusters } = require("../services/clusterService");
+
+const getClusteredCustomers = async (req, res) => {
+  try {
+    const maxCustomersPerCluster = parseInt(req.query.maxCustomersPerCluster) || 20;
+
+    const clusteredCustomers = await getClusteredCustomerLocations(maxCustomersPerCluster);
+
+    return successResponse(res, "Customers clustered successfully", null, clusteredCustomers);
+  } catch (error) {
+    console.log("Hello", error.message)
+    return errorResponse(res, error.message || error, 500);
+  }
+};
+
+const getClusters = async (req, res) => {
+  try {
+    const clusters = await getAllClusters();
+
+    return successResponse(res, "Clusters fetched successfully", null, clusters);
+  } catch (error) {
+    return errorResponse(res, "Failed to fetch clusters", 500, error.message || error);
+  }
+};
+
+const reassignMultipleCustomers = async (req, res) => {
+  try {
+    const { reassignments } = req.body;
+
+    if (!Array.isArray(reassignments)) {
+      return errorResponse(res, "Reassignments array is required.", 400); 
+    }
+
+    await reassignMultipleCustomersToClusters(reassignments);
+    return successResponse(res, "Customers reassigned successfully");
+  } catch (error) {
+    return errorResponse(res, error.message, 500);
+  }
+};
+
+module.exports = {reassignMultipleCustomers, getClusters, getClusteredCustomers }
--- controllers/customerControllers.js
+++ controllers/customerControllers.js
@@ -154,44 +154,6 @@ const getCustomerlocation = async (req, res) => {
   }
 };
 
-const getClusteredCustomers = async (req, res) => {
-  try {
-    const maxCustomersPerCluster = parseInt(req.query.maxCustomersPerCluster) || 15;
-
-    const clusteredCustomers = await getClusteredCustomerLocations(maxCustomersPerCluster);
-
-    return successResponse(res, "Customers clustered successfully", null, clusteredCustomers);
-  } catch (error) {
-    return errorResponse(res, "Error clustering customers", 500, error.message || error);
-  }
-};
-
-const getClusters = async (req, res) => {
-  try {
-    const clusters = await getAllClusters();
-
-    return successResponse(res, "Clusters fetched successfully", null, clusters);
-  } catch (error) {
-    return errorResponse(res, "Failed to fetch clusters", 500, error.message || error);
-  }
-};
-
-const reassignMultipleCustomers = async (req, res) => {
-  try {
-    const { reassignments } = req.body;
-
-    if (!Array.isArray(reassignments)) {
-      return errorResponse(res, "Reassignments array is required.", 400); 
-    }
-
-    await reassignMultipleCustomersToClusters(reassignments);
-    return successResponse(res, 200, "Customers reassigned successfully.");
-  } catch (error) {
-    return errorResponse(res, 500, error.message);
-  }
-};
-
-
 const sendCartidgeMissedMsg = async (req, res) => {
   try{
      const customer_id = req.body.customer_id;
@@ -226,4 +188,4 @@ return errorResponse(res, "Error while geting Logs", 500, error);
 };
 
 
-module.exports = { getCustomerdropdown, storeCustomers, getCustomers, getCustomerByCode,  ZohoCustomers, ManageCustomerAndProducts, getCustomerlocation, sendCartidgeMissedMsg, MissedCartidgeLog, getClusteredCustomers, getClusters, reassignMultipleCustomers };
+module.exports = { getCustomerdropdown, storeCustomers, getCustomers, getCustomerByCode,  ZohoCustomers, ManageCustomerAndProducts, getCustomerlocation, sendCartidgeMissedMsg, MissedCartidgeLog };
