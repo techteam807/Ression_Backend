8f5094950686ee58c86a2884177e83a0595d8515
techteam807 <techcenter@doshionpoly.com>
2025-05-08 11:02:23 +0530

    Cluster merge

--- services/clusterService.js
+++ services/clusterService.js
@@ -2,7 +2,7 @@ const Customer = require("../models/customerModel");
 const Cluster = require("../models/clusterModel.js");
 const mongoose = require('mongoose');
 
-const distance = (coord1, coord2) => {
+const distance = (coord1, coord2) => { 
     const dx = coord1[0] - coord2[0];
     const dy = coord1[1] - coord2[1];
     return Math.sqrt(dx * dx + dy * dy);
