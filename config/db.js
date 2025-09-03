ea09571a701d39646e3908eef8b6911c96aa306a
infolanze807 <techteam.infolanze@gmail.com>
2025-09-03 15:44:53 +0530

    updated db

--- config/db.js
+++ config/db.js
@@ -25,7 +25,8 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    });
+       serverSelectionTimeoutMS: 30000,     
+      });
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error("MongoDB Connection Failed:", error.message);
